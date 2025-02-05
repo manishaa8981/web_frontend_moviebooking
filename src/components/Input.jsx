e50f37c275c9a15e4109c015caf234549677ae8b
manishaa8981 <manishaa8981@gmail.com>
2025-02-05 19:12:55 +0545

    removed unnecessary files

--- src/components/Input.jsx
+++
@@ -1,9 +0,0 @@
-// input.jsx
-import React from "react";
-
-export const Input = ({ className, ...props }) => (
-  <input
-    className={`border border-gray-300 rounded px-3 py-2 w-full ${className}`}
-    {...props}
-  />
-);
